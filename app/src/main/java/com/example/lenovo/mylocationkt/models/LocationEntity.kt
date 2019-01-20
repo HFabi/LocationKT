88db99f8e08da26c44e6552eba56c0fe5150d1d7
fabian h <fab.h@gmx.de>
2019-01-20 19:59:22 +0100

    Add comments

--- app/src/main/java/com/example/lenovo/mylocationkt/models/LocationEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lenovo.mylocationkt.models
-
-class LocationEntity(
-    var title: String,
-    var longitude: Double,
-    var latitude: Double,
-    var addressLine: String,
-    var countryName: String
-)
